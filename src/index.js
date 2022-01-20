78a51942043319d5b446f8b068ba333b566973ad
Daniel Monterde <dn.monterde@gmail.com>
2022-01-20 16:45:02 +0800

    additional changes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import "./index.css"
 
 //My first component
 
+function TasksList() {
+  return (
+    <ul>
+      <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+      <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+      <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+    </ul>
+  );
+}
+
 function App() {
+
+	const paraStyle = {
+		"marginBottom":"15px"
+	};
   return (
     <div className="app">
       <header className="app-header">
         <h1>My Amazing ToDo-List App</h1>
-        <p>The most simple and amazing todo-list React app.</p>
+        <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
+        <TasksList/>
       </header>
     </div>
   );

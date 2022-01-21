c5b3e2284739207d593f24033cc1b2d6857d7363
Daniel Monterde <dn.monterde@gmail.com>
2022-01-21 15:00:52 +0800

    Create the inputTaskHandler and useState hook

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,7 @@
 import Task from './Task';
 
+import {useState} from 'react';
+
 function TasksList() {
 
   const taskItemList = [
@@ -7,9 +9,22 @@ function TasksList() {
     "Follow AWS Siklab PH on Facebook.",
     "Follow Zuitt Coding Bootcamp on Facebook."
   ];
+
+
+  const[taskValue, setTaskValue] = useState("Just another Task");
+  console.log("taskValue", taskValue);
+
+  const inputChangeHandler = (e)=>{
+    setTaskValue(e.target.value);
+  }
+
   return (
     <div>
-    <input className="task-input"/>
+      <input
+        className="task-input"
+        placeholder="Create a new task"
+        onChange={(e) => inputChangeHandler(e)}
+      />
     <ul>
       {
         taskItemList.map((task,index)=>{
